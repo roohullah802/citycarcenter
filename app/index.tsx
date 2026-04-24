7493e4473ebe4c87a1f587af84695676437f453b
roohullah802 <jaanroohullah83@gmail.com>
2026-04-23 20:33:37 -0700

    bug fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,12 @@
+import { useAuth } from "@clerk/expo";
 import { Redirect } from "expo-router";
 
 export default function Index() {
+  const { isSignedIn } = useAuth();
+
+  if (!isSignedIn) {
+    return <Redirect href="/screens/Auth/SocialAuth" />;
+  }
+
   return <Redirect href="/(tabs)/Home" />;
 }
