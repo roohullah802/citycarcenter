7a0d612c6f7e3803606bcd5ef90ac9091a438f82
roohullah802 <jaanroohullah83@gmail.com>
2026-05-07 06:18:43 -0700

    changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,6 +20,9 @@ export const unstable_settings = {
 };
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+const publishableStripeKey = process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+console.log("kk ", publishableStripeKey);
+
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
@@ -59,32 +62,32 @@ export default function RootLayout() {
               <ToastProvider>
                 <StripeProvider
                   publishableKey={
-                    process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+                    publishableStripeKey
                   }
                 >
-                    <Stack
-                      screenOptions={{ 
-                        headerShown: false, 
-                        animation: "fade_from_bottom" 
-                      }}
-                    >
-                      <Stack.Screen name="(tabs)" />
-                      <Stack.Screen name="screens/Others/BrandCards" />
-                      <Stack.Screen name="screens/Others/SearchCarCards" />
-                      <Stack.Screen name="screens/Others/CarCardsByBrand" />
-                      <Stack.Screen name="screens/Setting/PrivatePolicy" />
-                      <Stack.Screen name="screens/Setting/Report" />
-                      <Stack.Screen name="screens/Lease/ExtendLease" />
-                      <Stack.Screen name="screens/Others/CarLeaseDetails" />
-                      <Stack.Screen name="screens/Others/DateAndTime" />
-                      <Stack.Screen name="screens/Lease/LeaseDetails" />
-                      <Stack.Screen name="screens/Payments/PaymentDetails" />
-                      <Stack.Screen name="screens/Payments/PaymentSuccess" />
-                      <Stack.Screen name="screens/Auth/SocialAuth" />
-                      <Stack.Screen name="screens/Others/CarImages" />
-                      <Stack.Screen name="screens/Setting/DocumentUploadScreen" />
-                      <Stack.Screen name="screens/Setting/DocumentSubmittedScreen" />
-                    </Stack>
+                  <Stack
+                    screenOptions={{
+                      headerShown: false,
+                      animation: "fade_from_bottom"
+                    }}
+                  >
+                    <Stack.Screen name="(tabs)" />
+                    <Stack.Screen name="screens/Others/BrandCards" />
+                    <Stack.Screen name="screens/Others/SearchCarCards" />
+                    <Stack.Screen name="screens/Others/CarCardsByBrand" />
+                    <Stack.Screen name="screens/Setting/PrivatePolicy" />
+                    <Stack.Screen name="screens/Setting/Report" />
+                    <Stack.Screen name="screens/Lease/ExtendLease" />
+                    <Stack.Screen name="screens/Others/CarLeaseDetails" />
+                    <Stack.Screen name="screens/Others/DateAndTime" />
+                    <Stack.Screen name="screens/Lease/LeaseDetails" />
+                    <Stack.Screen name="screens/Payments/PaymentDetails" />
+                    <Stack.Screen name="screens/Payments/PaymentSuccess" />
+                    <Stack.Screen name="screens/Auth/SocialAuth" />
+                    <Stack.Screen name="screens/Others/CarImages" />
+                    <Stack.Screen name="screens/Setting/DocumentUploadScreen" />
+                    <Stack.Screen name="screens/Setting/DocumentSubmittedScreen" />
+                  </Stack>
                 </StripeProvider>
               </ToastProvider>
             </QueryClientProvider>
